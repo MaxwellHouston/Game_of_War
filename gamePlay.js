9cfba56208bdcf15f16994d99e318dac756f2f64
Max Houston <maxhouston@gmail.com>
2021-07-08 00:11:09 -0400

    Updated the gamePlay folder to shuffle, draw the cards, and play a hand

--- gamePlay.js
+++ gamePlay.js
@@ -3,7 +3,7 @@
 const {deck} = require('./deckBuilder.js');
 const playerDeck = [];
 const computerDeck = [];
-
+const table = [];
 
 // Shuffles the deck
 const shuffleDeck = (array) =>{
@@ -32,6 +32,47 @@ const splitDeck = (shuffledDeck) =>{
 } 
 splitDeck(deck);
 
-console.log(playerDeck.length)
-console.log(computerDeck.length)
+
+// Draws one card from each deck and puts them on the table
+const drawPhase = () =>{
+    table.push(playerDeck.shift());
+    table.push(computerDeck.shift());
+}
+drawPhase();
+
+console.log(table);
+
+const playPhase = () =>{
+    if(table[0].number > table[1].number){
+    console.log(`Your ${table[0].name} beats the computer's ${table[1].name}!`);
+    playerDeck.push(table[0]);
+    playerDeck.push(table[1]);
+    table.splice(0,2);
+    }
+    else if( table[0].number < table[1].number){
+    console.log(`Your ${table[0].name} loses to the computer's ${table[1].name}!`);
+    computerDeck.push(table[0]);
+    computerDeck.push(table[1]);
+    table.splice(0,2);
+    } 
+    else{
+    console.log(`Your ${table[0].name} ties the computer's ${table[1].name}!`);
+    playerDeck.push(table[0]);
+    computerDeck.push(table[1]);
+    table.splice(0,2);
+    }  
+}
+playPhase();
+console.log(table);
+console.log(playerDeck.length);
+console.log(computerDeck.length);
+
+console.log('Your last two cards')
+console.log(playerDeck[playerDeck.length-1])
+console.log(playerDeck[playerDeck.length-2])
+
+console.log('their last two cards')
+console.log(computerDeck[computerDeck.length-1])
+
+console.log(computerDeck[computerDeck.length-2])
 
