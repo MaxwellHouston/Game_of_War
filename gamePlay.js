c2fe044a9c0fb286f336059389eea0c53b4e2ee9
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2021-07-09 00:22:24 -0400

    started work on the tie scenario. still need to fix the return on a double tie

--- gamePlay.js
+++ gamePlay.js
@@ -4,6 +4,9 @@ const {deck} = require('./deckBuilder.js');
 const playerDeck = [];
 const computerDeck = [];
 const table = [];
+let tieStatus = true;
+let player3 = [];
+let computer3 = [];
 
 // Shuffles the deck
 const shuffleDeck = (array) =>{
@@ -40,39 +43,77 @@ const drawPhase = () =>{
 }
 drawPhase();
 
-console.log(table);
 
+
+const tiePlay = () =>{
+    player3 = playerDeck.splice(0,3);
+    computer3 = computerDeck.splice(0,3);
+    if(player3[2].number > computer3[2].number){
+        console.log(`Your ${player3[2].name} beats the computer's ${computer3[2].name}!
+        That's a good haul!`);
+      tieStatus = 'player'
+    }
+    else if (player3[2].number < computer3[2].number){
+        console.log(`Your ${player3[2].name} loses to the computer's ${computer3[2].name}!
+        That's a big loss!`); 
+        tieStatus = 'computer'
+        
+    }
+    else{
+        console.log(`Your ${player3[2].name} ties to the computer's ${computer3[2].name}!
+        Let's up the ante!`)
+        tieStatus = false;
+    }
+
+}
 const playPhase = () =>{
     if(table[0].number > table[1].number){
     console.log(`Your ${table[0].name} beats the computer's ${table[1].name}!`);
     playerDeck.push(table[0]);
     playerDeck.push(table[1]);
     table.splice(0,2);
+
     }
     else if( table[0].number < table[1].number){
     console.log(`Your ${table[0].name} loses to the computer's ${table[1].name}!`);
     computerDeck.push(table[0]);
     computerDeck.push(table[1]);
     table.splice(0,2);
+   
     } 
     else{
-    console.log(`Your ${table[0].name} ties the computer's ${table[1].name}!`);
-    playerDeck.push(table[0]);
-    computerDeck.push(table[1]);
-    table.splice(0,2);
+    console.log(`Your ${table[0].name} ties the computer's ${table[1].name}! Now
+    each player puts three cards facedown and turns the last one up. Good luck!`);
     }  
 }
-playPhase();
-console.log(table);
-console.log(playerDeck.length);
-console.log(computerDeck.length);
 
-console.log('Your last two cards')
-console.log(playerDeck[playerDeck.length-1])
-console.log(playerDeck[playerDeck.length-2])
-
-console.log('their last two cards')
-console.log(computerDeck[computerDeck.length-1])
-
-console.log(computerDeck[computerDeck.length-2])
-
+const trial = () =>{
+    tiePlay();
+    if(tieStatus==='player'){
+        for(let i = 0; i < player3.length; i++){
+            playerDeck.push(player3[i]);
+            playerDeck.push(computer3[i]);
+        }
+        playerDeck.push(table[0]);
+        playerDeck.push(table[1]);
+        table.splice(0,2);
+    }
+    else if ( tieStatus==='computer'){
+        for(let i = 0; i < player3.length; i++){
+            computerDeck.push(player3[i]);
+            computerDeck.push(computer3[i]);
+        }
+        computerDeck.push(table[0]);
+        computerDeck.push(table[1]);
+        table.splice(0,2);   
+    }
+    else if ( tieStatus===false){
+        trial()
+    }
+}
+console.log(playerDeck.length)
+        console.log(computerDeck.length)
+trial()
+        console.log(playerDeck.length)
+        console.log(computerDeck.length)
+      
